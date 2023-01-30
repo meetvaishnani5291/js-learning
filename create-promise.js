fc906c853c7f6c5c1ac942949abf35a1ff130342
meetvaishnani5291 <meet.vaishnani@simformsolutions.com>
2023-01-30 14:28:03 +0530

    performed practical on promise-chaining

--- create-promise.js
+++ create-promise.js
@@ -1,28 +1,48 @@
 x = 3.145;
 
 function myfun1() {
-    console.log("1");
+  console.log("1");
 }
-myfun1();
+// myfun1();
 function myownwait(seconds) {
-    console.log("2");
-    myPromise = new Promise(function (resolve, reject) {
-        console.log("3");
-        if (!isNaN(seconds)) {
-            setTimeout(() => resolve("hii after " + seconds), seconds * 1000);
-        } else {
-            myerr = Error("not a number.");
-            reject(myerr);
-        }
-    });
-    return myPromise;
+  //   console.log("2");
+  myPromise = new Promise(function (resolve, reject) {
+    // console.log("3");
+    if (!isNaN(seconds)) {
+      setTimeout(() => resolve("hii after " + seconds), seconds * 1000);
+    } else {
+      myerr = Error("not a number.");
+      reject(myerr);
+    }
+  });
+  return myPromise;
 }
 
-myownwait(2)
-    .then((res) => console.log(res))
-    .catch((err) => console.log(err));
+// myownwait(2)
+//     .then((res) => res+1)
+//     .then(res => console.log(res))
+//     .catch((err) => console.log(err));
 
+console.log(
+  myownwait("dd")
+    .then(() => {
+      throw "err1";
+      return 1;
+    })
+    .then(() => {
+      console.log("this will never displayed.");
+    })
+    .catch(function (err) {
+      console.log(err);
+    })
+  // .then((data) => {
+  //   console.log(data);
+  //   return myownwait("mm");
+  // })
+  // .catch(function (err) {
+  //     console.log("hi from catch"+err);
+  // })
+);
 
 myfun1();
 console.log("4");
-
